cdd675415f51e65265dc9212b77bcccbf6c84bdc
elogeek <elodiechristin@gmail.com>
2021-03-22 13:58:29 +0100

    start

---
+++ app.js
@@ -0,0 +1,12 @@
+let game = document.querySelector('canvas');
+
+// Draw Food
+let arrayFood = [document.getElementsByClassName(".food")];
+
+
+/**
+ * Window Load
+ */
+window.onload = function() {
+    window.reload();
+};
